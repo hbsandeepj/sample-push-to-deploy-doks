527b3e7201175e42a9057299821ecb4611b905ff
sandeep.jpatel <sandeep.jpatel@hiddenbrains.in>
2024-03-28 21:26:29 +0530

    updated text

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     html = """Hello {name}!
-    Hostname: {hostname}"""
+    Hostname: {hostname} This is an automated message!!"""
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
 
 if __name__ == "__main__":
